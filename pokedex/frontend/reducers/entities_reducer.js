6df3e6df48f99cbbd137d9e77d5c78790ea24d38
chuuster <chu.christina@yahoo.com>
2018-12-05 17:31:15 -0800

    final commit

--- pokedex/frontend/reducers/entities_reducer.js
+++ pokedex/frontend/reducers/entities_reducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+import { pokemonReducer } from "./pokemon_reducer";
 
 const entitiesReducer = combineReducers({
   pokemon: pokemonReducer
--- pokedex/frontend/store/store.js
+++ pokedex/frontend/store/store.js
@@ -2,5 +2,6 @@ import { createStore, applyMiddlware } from "redux";
 import rootReducer from "../reducers/root_reducer";
 import logger from "redux-logger";
 
-export const configureStore = () =>
-  createStore(rootReducer, applyMiddlware(logger));
+export const configureStore = function() {
+  return createStore(rootReducer, applyMiddlware(logger));
+};
